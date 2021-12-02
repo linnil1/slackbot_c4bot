bb6aa2cb6a24ef15bf6cfb031e4a2e48aa24a40a
linnil1 <linnil1.886@gmail.com>
2021-12-02 23:57:20 +0800

    Rewrite emoji word

--- app.py
+++ app.py
@@ -1,54 +1,48 @@
-from flask import Flask, request
-from pprint import pprint
-from slackeventsapi import SlackEventAdapter
-import json
 import os
+# from aiohttp import web
+from slack_bolt.async_app import AsyncApp
+import logging
 
 import configuration
+from emoji_word import Emoji
+logging.basicConfig(level=logging.DEBUG)
 
 
-app = Flask(__name__)
-sercet = configuration.signing_secret
-events = SlackEventAdapter(sercet, "/slack", app)
+app = AsyncApp(token=configuration.token,
+               signing_secret=configuration.signing_secret)
 
 
-# Create an event listener for "reaction_added" events and print the emoji name
-@events.on("reaction_added")
-def reaction_added(data):
-    """Do nothing"""
-    pprint(data)
+@app.event("message")
+async def event_read_message(body, say, logger):
+    logger.info(body)
+    pass
+    # await say("What's up?")
 
 
-@events.on("message")
-def message(data):
-    """Record all the message to queue"""
-    print("Get message")
-    pprint(data)
-    message = data["event"]
-    if not message:
-        return "OK"
-    name = message["ts"]
-    json.dump(message, open(f"queue/{name}.json", "w"))
-    return "OK"
+@app.event("app_home_opened")
+async def handle_app_home_opened_events(body, say, logger):
+    logger.info(body)
 
 
-@app.route("/command", methods=["POST"])
-def command():
-    data = dict(request.form)
-    print(data)
-    if not data.get('text'):
-        return "Not text"
-    # save to queue
-    data["type"] = "command"
-    name = data['trigger_id']
-    json.dump(data, open(f"queue/{name}.json", "w"))
-    return ""
+@app.command("/emojiword")
+async def command_emojiword(ack, respond, command, say, logger):
+    logger.info(respond)
+    logger.info(command)
+    command = command['text']
+    await ack()
+    if len(command.split()) < 2:
+        await say("Error format. (Correct one: emojiword broken 壞了)")
+        return
+    name, word = command.split(maxsplit=1)
+    try:
+        em = Emoji(name, word)
+        await em.upload()
+    except BaseException as e:
+        await say(f"Error {str(e)}")
+        return
+
+    await say(f"OK Here you are :{em.name}:")
 
 
-# run
-os.makedirs("queue", exist_ok=True)
-os.makedirs("queue_fail", exist_ok=True)
-app.run(host="0.0.0.0",
-        port=configuration.web_port,
-        ssl_context=configuration.ssl_context,
-        debug=True)
+if __name__ == "__main__":
+    app.start(port=configuration.web_port)
--- emoji_word.py
+++ emoji_word.py
@@ -1,47 +1,93 @@
-import requests
-import slack
+import os
+import re
+import copy
+import aiohttp
+from slack_sdk.web.async_client import AsyncWebClient
 
 import configuration
 
-url = "https://emoji-gen.ninja/emoji_download"
-params = {'align': "center",
-          'back_color': "00000000",
-          'color': "EC71A1FF",
-          'font': "gensenmarugothic_tw_ttf_bold",
-          'locale': "zh-Hant",
-          'public_fg': "true",
-          'size_fixed': "false",
-          'stretch': "false",
-          'text': ""
-}
-token = configuration.user_token
-client = slack.WebClient(token=token)
-filename = "data/tmp_emojiword.jpg"
 
+class Emoji:
+    """
+    Create a pretty work image as emoji
+    """
+    url = "https://emoji-gen.ninja/emoji_download"
+    url_params = {
+        'align': "center",
+        'back_color': "00000000",
+        'color': "EC71A1FF",
+        'font': "gensenmarugothic_tw_ttf_bold",
+        'locale': "zh-Hant",
+        'public_fg': "true",
+        'size_fixed': "false",
+        'stretch': "false",
+    }
+    rename_style = "bot-{}"
+    folder = "emojis/"
+    image_format = ".jpg"
+    client_user = AsyncWebClient(token=configuration.user_token)
+    client = AsyncWebClient(token=configuration.token)
 
-def imageDownload(url, params):
-    r = requests.get(url, params=params, stream=True)
-    if r.status_code == 200:
-        with open(filename, 'wb') as f:
-            for chunk in r.iter_content(1024):
-                f.write(chunk)
-    else:
-        raise ValueError("Cannot download")
+    def __init__(self, name, word):
+        """ 
+        Parameter:
+          name(str): filename-like string
+          word(str): The text you want to generate, 
+        """
+        if not re.match(r"^\w+$", name):
+            raise ValueError(f"Bad Name: {name}")
 
+        self.name = self.rename_style.format(name)
+        self.fname = os.path.join(self.folder, self.name) + self.image_format
+        self.word = word
+        os.makedirs(self.folder, exist_ok=True)
 
-def emojiUpload(name, text):
-    params['text'] = text
-    imageDownload(url, params)
+    async def imageDownload(self):
+        """ Download image of the word """
+        print(f"Download {self.word}...")
+        self.params = copy.deepcopy(self.url_params)
+        self.params['text'] = self.word
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.url, params=self.params) as resp:
+                with open(self.fname, 'wb') as fd:
+                    async for chunk in resp.content.iter_chunked(128):
+                        fd.write(chunk)
+        return True
 
-    # upload to slack emoji
-    response = client.api_call(
+    async def emojiUpload(self):
+        """ upload to slack emoji """
+        print(f"Upload {self.name}(self.word)...")
+        response = await self.client_user.api_call(
             "emoji.add",
             data={
-                'name': name,
-                'mode': 'data'},
-            files={'image': filename})
-    assert(response["ok"])
+                'name': self.name,
+                'mode': 'data',
+                '_x_reason': "customize-emoji-add",
+                '_x_mode': "online",
+            },
+            headers={"Cookie": configuration.cookie},
+            files={'image': self.fname})
+        print(response)
+        return True
+
+    async def isExist(self):
+        return os.path.exists(self.fname)
+
+    async def isUpload(self):
+        response = await self.client.emoji_list() 
+        emojis = response.data['emoji']
+        return self.name in emojis.keys()
+
+    async def upload(self, force=False):
+        """ Run all the function """
+        if force or not await self.isExist():
+            await self.imageDownload()
+        if force or not await self.isUpload():
+            await self.emojiUpload()
 
 
 if __name__ == "__main__":
-    emojiUpload("no", "不")
+    # test
+    import asyncio
+    a = Emoji("broken", "壞了")
+    asyncio.run(a.upload(force=True))
